b2df51aed5c41778e6df2d7e2b08764da0352838
ienacar <ibrahimethemnacar@gmail.com>
2020-11-11 16:43:40 +0200

    data driven tests added.

---
+++ UnitTests/Reservation/ReservationTicketPriceCalculatorShould.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using NunitDemoLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Reservation
+{
+    public class ReservationTicketPriceCalculatorShould
+    {
+        [Test]
+        public void ReturnOneTermPrice()
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            var payment = calculator.CalculateTicketPrice(100,1,0.02M);
+            //assert
+            Assert.That(payment, Is.EqualTo(102));
+        }
+
+        [Test]
+        public void ReturnMultiTermPrice()
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            var payment = calculator.CalculateTicketPrice(100, 10, 0.02M);
+            //assert
+            Assert.That(payment, Is.EqualTo(10.2));
+        }
+
+        [Test]
+        public void ReturnMultiTermPriceWithRatev2()
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            var payment = calculator.CalculateTicketPrice(100, 10, 0.03M);
+            //assert
+            Assert.That(payment, Is.EqualTo(10.3));
+        }
+
+        [Test]
+        [TestCase(100,1,0.02,ExpectedResult = 102)]
+        [TestCase(100, 10, 0.02, ExpectedResult = 10.2)]
+        [TestCase(100, 10, 0.03, ExpectedResult = 10.3)]
+        public decimal ReturnPriceWithMultiCases(decimal price,int term, decimal rate)
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            return calculator.CalculateTicketPrice(price, term, rate);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TicketPriceData), "TestCases")]
+        public void ReturnPriceWithMultiCases_FromClass(decimal price, int term, decimal rate,decimal expectedValue)
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            var payment = calculator.CalculateTicketPrice(price, term, rate);
+            //
+            Assert.That(payment,Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TicketPriceData), "TestCasesWithReturn")]
+        public decimal ReturnPriceWithMultiCases_FromClassWithReturnValues(decimal price, int term, decimal rate)
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            return calculator.CalculateTicketPrice(price, term, rate);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TicketPriceData), "CasesFromCsvFile",new object[] { @"D:\Workspace\AlbUnitTestDemo\UnitTests\Reservation\Data.csv" })]
+        public void ReturnPriceWithMultiCases_FromCsvFile(decimal price, int term, decimal rate, decimal expectedValue)
+        {
+            //arrange
+            var calculator = new ReservationTicketPriceCalculator();
+            //act
+            var payment = calculator.CalculateTicketPrice(price, term, rate);
+            //assert
+            Assert.That(payment, Is.EqualTo(expectedValue));
+        }
+
+    }
+}
---
+++ UnitTests/Reservation/TicketPriceData.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Reservation
+{
+    public class TicketPriceData
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(100m, 1, 0.02m, 102m);
+                yield return new TestCaseData(100m, 10, 0.02m, 10.2m);
+                yield return new TestCaseData(100m, 10, 0.03m, 10.3m);
+                yield return new TestCaseData(100m, 1, 0.03m, 103m);
+            }
+        }
+
+        public static IEnumerable TestCasesWithReturn
+        {
+            get
+            {
+                yield return new TestCaseData(100m, 1, 0.02m).Returns(102m);
+                yield return new TestCaseData(100m, 10, 0.02m).Returns(10.2m);
+                yield return new TestCaseData(100m, 10, 0.03m).Returns(10.3m);
+                yield return new TestCaseData(100m, 1, 0.03m).Returns(103m);
+            }
+        }
+
+        public static IEnumerable CasesFromCsvFile(string csvFile)
+        {
+            var csvLines = File.ReadAllLines(csvFile);
+            var testCases = new List<TestCaseData>();
+
+            foreach (var line in csvLines)
+            {
+                string[] values = line.Replace(" ", "").Split('-');
+                decimal price = decimal.Parse(values[0]);
+                int term = int.Parse(values[1]);
+                decimal rate = decimal.Parse(values[2]);
+                decimal expectedValue = decimal.Parse(values[3]);
+
+                testCases.Add(new TestCaseData(price,term,rate,expectedValue));
+            }
+
+            return testCases;
+        }
+    }
+}
