c6d279e89a90778ded8a0c81b6bc89e5a5a198ec
ienacar <ibrahimethemnacar@gmail.com>
2020-11-13 11:20:13 +0200

    Tdd demo added.

---
+++ UnitTests/TddTests/FizzBuzzManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UnitTests.TddTests
+{
+    public class FizzBuzzManager
+    {
+        public FizzBuzzManager()
+        {
+        }
+
+        public string Print(int v)
+        {
+            if (IsFizz(v) && IsBuzz(v))
+            {
+                return "FizzBuzz";
+            }
+            else if (IsFizz(v))
+            {
+                return "Fizz";
+            }
+            else if(IsBuzz(v))
+            {
+                return "Buzz";
+            }
+            return v.ToString();
+        }
+
+        private bool IsFizz(int val)
+        {
+            if (val % 3 == 0) return true;
+            return false;
+        }
+
+        private bool IsBuzz(int val)
+        {
+            if (val % 5 == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ UnitTests/TddTests/FizzBuzzManagerShould.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.TddTests
+{
+    public class FizzBuzzManagerShould
+    {
+        [Test]
+        public void PrintNumber()
+        {
+            //arrange
+            var fizzBuzzManager = new FizzBuzzManager();
+            //act
+            var returnValue = fizzBuzzManager.Print(1);
+            //assert
+            Assert.That(returnValue,Is.EqualTo("1"));
+        }
+
+        [Test]
+        [TestCase(3,ExpectedResult = "Fizz")]
+        [TestCase(6, ExpectedResult = "Fizz")]
+        public string PrintFizz(int val)
+        {
+            //arrange
+            var fizzBuzzManager = new FizzBuzzManager();
+            //act
+            return fizzBuzzManager.Print(val);
+        }
+
+        [Test]
+        [TestCase(5, ExpectedResult = "Buzz")]
+        [TestCase(10, ExpectedResult = "Buzz")]
+        public string PrintBuzz(int val)
+        {
+            //arrange
+            var fizzBuzzManager = new FizzBuzzManager();
+            //act
+            return fizzBuzzManager.Print(val);
+        }
+
+        [Test]
+        [TestCase(15, ExpectedResult = "FizzBuzz")]
+        public string PrintFizzBuzz(int val)
+        {
+            //arrange
+            var fizzBuzzManager = new FizzBuzzManager();
+            //act
+            return fizzBuzzManager.Print(val);
+        }
+
+    }
+}
